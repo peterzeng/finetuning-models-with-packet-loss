f71da989d5e18520bbd1a5a2e2d7089468225aa0
pegah <palipoormola@cs.stonybrook.edu>
2025-04-13 20:07:19 -0400

    add more code - not tested

---
+++ src/main.py
@@ -0,0 +1,63 @@
+from comms import LossyNetwork
+from trainer import DistributedTrainer
+from datasets import load_dataset
+from transformers import TrainingArguments, AutoModelForSequenceClassification, AutoTokenizer
+
+def main(args):
+    network = LossyNetwork(loss_rate=args.loss_rate)
+    network.set_seed(args.seed)
+
+    dataset = load_dataset("allenai/winogrande", "winogrande_l", trust_remote_code=True)    
+    train_dataset = dataset["train"]
+    eval_dataset = dataset["validation"]
+    model = AutoModelForSequenceClassification.from_pretrained("google-bert/bert-large-uncased", num_labels=2)
+    tokenizer = AutoTokenizer.from_pretrained("google-bert/bert-large-uncased")
+
+    def preprocess(data):
+        question = data["sentence"] + "\n" + data["option1"] + "\n" + data["option2"]
+        label = 0 if data["answer"] == '1' else 1
+        return {
+            'input_ids': tokenizer(question, truncation=True, padding="max_length", max_length=512)["input_ids"],
+            'attention_mask': tokenizer(question, truncation=True, padding="max_length", max_length=512)["attention_mask"],
+            'labels': label
+        }
+    
+    train_dataset = train_dataset.map(preprocess, remove_columns=["sentence", "option1", "option2", "answer"])
+    eval_dataset = eval_dataset.map(preprocess, remove_columns=["sentence", "option1", "option2", "answer"])
+
+    training_args = TrainingArguments(
+        output_dir=args.output_dir,
+        per_device_train_batch_size=args.batch_size,
+        per_device_eval_batch_size=args.batch_size,
+        num_train_epochs=3,
+        evaluation_strategy="epoch",
+        save_strategy="epoch",
+        logging_dir=f"{args.output_dir}/logs",
+        logging_steps=10,
+        fp16=args.fp16,
+    )
+    trainer = DistributedTrainer(
+        num_nodes=args.num_nodes,
+        network=network,
+        model=model,
+        tokenizer="google-bert/bert-large-uncased",
+        args = training_args,
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
+    )
+
+    trainer.train()
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description="Distributed Training with Packet Loss")
+    parser.add_argument('--num_nodes', type=int, default=2, help='Number of nodes')
+    parser.add_argument('--loss_rate', type=float, default=0.01, help='Packet loss rate')
+    parser.add_argument('--seed', type=int, default=1234, help='Random seed')
+    parser.add_argument('--model_name', type=str, default='openai-community/gpt2', help='Model name')
+    parser.add_argument('--batch_size', type=int, default=32, help='Batch size')
+    parser.add_argument('--fp16', action='store_true', help='Use mixed precision training')
+    parser.add_argument('--output_dir', type=str, default='./output', help='Output directory')
+    args = parser.parse_args()
+    
+    main(args)
--- src/trainer.py
+++ src/trainer.py
@@ -2,6 +2,7 @@ from transformers import Trainer, TrainingArguments
 import torch
 
 class DistributedTrainer(Trainer):
+
     def __init__(self, num_nodes, network, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.num_nodes = num_nodes
@@ -9,9 +10,10 @@ class DistributedTrainer(Trainer):
         self.network.set_seed(1234)
 
 
-    def training_step(self, model, inputs, num_items_in_batch=None):
+    def training_step(self, model, inputs):
+        num_items_in_batch = len(inputs[list(inputs.keys())[0]])
         minibatch_size = num_items_in_batch // self.num_nodes
-        gradients = []
+        averaged_gradients = {k:torch.zeros_like(v) for k, v in model.named_parameters()}
         for i in range(self.num_nodes):
             inputs_split = {k: v[i * minibatch_size:(i + 1) * minibatch_size] for k, v in inputs.items()}
             loss = super().training_step(model, inputs_split)
@@ -22,15 +24,13 @@ class DistributedTrainer(Trainer):
             for k,v in node_gradients.items():
                 mask = self.network.send(v)
                 node_gradients[k] = self.network.receive(v, mask)
-
-            gradients.append(node_gradients)
+                averaged_gradients[k] = averaged_gradients[k] + node_gradients[k]
+            model.zero_grad()
+            
+        averaged_gradients = {k: v / self.num_nodes for k, v in averaged_gradients.items()}
         
-        # Average gradients across nodes
-        averaged_gradients = {}
         for name, param in model.named_parameters():
-            averaged_gradients[name] = torch.mean(torch.stack([grad[name] for grad in gradients]), dim=0)
             param.grad = averaged_gradients[name]
         
         return loss
-    
     
