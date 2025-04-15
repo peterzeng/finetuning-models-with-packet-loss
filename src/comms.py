a1415aaf8ea42f2b41fb186dcb8ec41940f38101
pegah <palipoormola@cs.stonybrook.edu>
2025-04-15 11:43:24 -0400

    debug loss application

--- src/comms.py
+++ src/comms.py
@@ -21,13 +21,13 @@ class LossyNetwork:
     
     def receive(self, data: torch.Tensor, packets_mask: torch.Tensor) -> torch.Tensor:
         num_packets = len(packets_mask)
-        num_bytes = data.numel()
+        number_per_packet = MAX_PAYLOAD_BYTES // data.element_size()
         received_data = data.clone()
         received_data = received_data.view(-1)
         for i in range(num_packets):
             if not packets_mask[i]:
-                start = i * MAX_PAYLOAD_BYTES
-                end = min(start + MAX_PAYLOAD_BYTES, num_bytes)
-                received_data[start:end] = 0
+                start = i * number_per_packet
+                end = min(start + number_per_packet, data.numel())
+                received_data[start:end] = 0.0
         received_data = received_data.view(data.shape)
         return received_data
