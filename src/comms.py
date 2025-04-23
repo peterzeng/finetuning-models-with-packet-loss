4bc571a4b4ef08ccd7420b9297fe55877e820539
pegah <palipoormola@cs.stonybrook.edu>
2025-04-22 20:06:53 -0400

    optimize receive function

--- src/comms.py
+++ src/comms.py
@@ -20,14 +20,25 @@ class LossyNetwork:
         return packets_mask
     
     def receive(self, data: torch.Tensor, packets_mask: torch.Tensor) -> torch.Tensor:
+
+        if packets_mask.all():
+            return data
         num_packets = len(packets_mask)
-        number_per_packet = MAX_PAYLOAD_BYTES // data.element_size()
-        received_data = data.clone()
-        received_data = received_data.view(-1)
-        for i in range(num_packets):
-            if not packets_mask[i]:
-                start = i * number_per_packet
-                end = min(start + number_per_packet, data.numel())
-                received_data[start:end] = 0.0
-        received_data = received_data.view(data.shape)
-        return received_data
+        number_per_packet = MAX_PAYLOAD_BYTES // data.element_size() + 1
+
+        flat = data.flatten()
+        indices = torch.arange(num_packets * number_per_packet, device=data.device)
+        indices = indices[indices < flat.numel()]
+        mask = packets_mask.repeat_interleave(number_per_packet)[:indices.numel()]
+        flat[~mask] = 0.0
+        return flat.view_as(data)
+    
+        # received_data = data.clone()
+        # received_data = received_data.view(-1)
+        # for i in range(num_packets):
+        #     if not packets_mask[i]:
+        #         start = i * number_per_packet
+        #         end = min(start + number_per_packet, data.numel())
+        #         received_data[start:end] = 0.0
+        # received_data = received_data.view(data.shape)
+        # return received_data
