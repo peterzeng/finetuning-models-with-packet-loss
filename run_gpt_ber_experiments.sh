b8b972a6df3554eda699bd636fd784d2255e95e9
pegah <palipoormola@cs.stonybrook.edu>
2025-06-20 16:42:20 -0400

    Add example running scripts

--- run_gpt_ber_experiments.sh
+++ run_gpt_ber_experiments.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 #MODEL="meta-llama/Llama-3.2-1B"
 MODEL="openai-community/gpt2-large"
-#MODEL="/data01/pegah/finetuning-models-with-packet-loss/gpt2-large_output/hotpotqa/gpt2-large_2nodes_hotpotqa_lr0.0_seed10/checkpoint-700"
 MODEL_ALIAS="gpt2-large"
-DATASET="squad"
+DATASET="mnli"
 
-LOSS_RATES=("0.0" "0.001" "0.002" "0.005" "0.01")
-NUM_NODES=("2" "8")
+LOSS_RATES=("0.0" "0.001" "0.005" "0.01")
+# LOSS_RATES=("0.002")
+# NUM_NODES=("2" "10")
+NUM_NODES=("8")
 SEEDS=("10" "20" "30" "40" "50")
+#SEEDS=("10")
 # GPU settings
-export CUDA_VISIBLE_DEVICES=2
+export CUDA_VISIBLE_DEVICES=0
 export WANDB_PROJECT="new_lossy_network"
 
 # Create output directory if it doesn't exist
@@ -27,15 +29,14 @@ for loss_rate in "${LOSS_RATES[@]}"; do
         --dataset "$DATASET" \
         --loss_rate "$loss_rate" \
         --num_nodes "$nodes" \
-        --batch_size $((2 * ${nodes})) \
+        --batch_size $((16 * ${nodes})) \
         --learning_rate 2e-5 \
         --run_id "$run_id" \
         --epochs 7 \
         --seed "$seed" \
         --output_dir "$output_dir" \
 	--eval_steps 20 \
-  
-	
+      
       echo "Completed experiment: $run_id"
       echo "--------------------------------"
     done
--- run_gpt_ge_experiments.sh
+++ run_gpt_ge_experiments.sh
@@ -1,41 +1,37 @@
 #!/bin/bash
 #MODEL="meta-llama/Llama-3.2-1B"
 MODEL="openai-community/gpt2-large"
-#MODEL="/data01/pegah/finetuning-models-with-packet-loss/gpt2-large_output/hotpotqa/gpt2-large_2nodes_hotpotqa_lr0.0_seed10/checkpoint-700"
 MODEL_ALIAS="gpt2-large"
-DATASET="squad"
+DATASET="mnli"
 
-LOSS_RATES=("0.0" "0.001" "0.002" "0.005" "0.01")
-NUM_NODES=("2" "8")
+# LOSS_RATES=("0.0" "0.001" "0.005" "0.01")
+NUM_NODES=("8")
 SEEDS=("10" "20" "30" "40" "50")
-# GPU settings
-export CUDA_VISIBLE_DEVICES=2
+CONFIGS=("short_1percent" "short_half_percent" "short_point_2percent")
+
+export CUDA_VISIBLE_DEVICES=1
 export WANDB_PROJECT="new_lossy_network"
 
-# Create output directory if it doesn't exist
-mkdir -p output
-
-for loss_rate in "${LOSS_RATES[@]}"; do
+for config in "${CONFIGS[@]}"; do
   for nodes in "${NUM_NODES[@]}"; do
     for seed in "${SEEDS[@]}"; do
-      run_id="${MODEL_ALIAS}_${nodes}nodes_${DATASET}_lr${loss_rate}_seed${seed}"
-      output_dir="${MODEL_ALIAS}_output/${DATASET}"
+      run_id="ge_${MODEL_ALIAS}_${nodes}nodes_${DATASET}_lr_${config}_seed${seed}"
+      output_dir="ge_${MODEL_ALIAS}_output/${DATASET}"
       echo "Starting experiment: $run_id"
       # Run the experiment
       python src/main.py \
         --model_name "$MODEL" \
         --dataset "$DATASET" \
-        --loss_rate "$loss_rate" \
         --num_nodes "$nodes" \
-        --batch_size $((2 * ${nodes})) \
+        --batch_size $((16 * ${nodes})) \
         --learning_rate 2e-5 \
         --run_id "$run_id" \
-        --epochs 7 \
+        --epochs 4 \
         --seed "$seed" \
         --output_dir "$output_dir" \
-	--eval_steps 20 \
-  
-	
+	      --eval_steps 20 \
+        --loss_type "g-e" \
+        --ge_config "$config" 
       echo "Completed experiment: $run_id"
       echo "--------------------------------"
     done
